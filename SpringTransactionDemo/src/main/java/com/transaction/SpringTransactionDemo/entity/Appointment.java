2368146af1b9dfd28c4eea9e25ea74a95aeaa4a8
Aritra Bhattacharya <57657953+BhattacharyAritra@users.noreply.github.com>
2021-08-13 16:40:15 +0530

    Spring Transaction Management Program

---
+++ SpringTransactionDemo/src/main/java/com/transaction/SpringTransactionDemo/entity/Appointment.java
@@ -0,0 +1,61 @@
+package com.transaction.SpringTransactionDemo.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+
+@Entity
+public class Appointment {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long AppointmentNumber;
+	private Date date;
+	private Long DoctorNumber;
+	private Long patientNumber;
+	public Appointment() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Appointment(Date date, Long doctorNumber, Long patientNumber) {
+		super();
+//		AppointmentNumber = appointmentNumber;
+		this.date = date;
+		DoctorNumber = doctorNumber;
+		this.patientNumber = patientNumber;
+	}
+	public Long getAppointmentNumber() {
+		return AppointmentNumber;
+	}
+	public void setAppointmentNumber(Long appointmentNumber) {
+		AppointmentNumber = appointmentNumber;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Long getDoctorNumber() {
+		return DoctorNumber;
+	}
+	public void setDoctorNumber(Long doctorNumber) {
+		DoctorNumber = doctorNumber;
+	}
+	public Long getPatientNumber() {
+		return patientNumber;
+	}
+	public void setPatientNumber(Long patientNumber) {
+		this.patientNumber = patientNumber;
+	}
+
+}
